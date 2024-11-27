3f55e1c079a360eb1c8b302143aba52fd546e55c
Muhammadismoil <karimovmuhammadismoil22@gmail.com>
2024-11-27 12:53:53 +0500

    added footer.component.jsx

---
+++ src/components/Footer.component.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className=" bg-blue-900 h-52 flex justify-center items-center mt-5">
+      <div>
+        <h1 className="text-white">Muhammadismoil Mirzorahim </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
