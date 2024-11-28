7b922006c6d1037de891755c21a80858c83c708e
Muhammadismoil <karimovmuhammadismoil22@gmail.com>
2024-11-28 12:58:42 +0500

    added the About page

---
+++ src/pages/About.page.tsx
@@ -0,0 +1,153 @@
+const About = () => {
+  return (
+    <div className="mt-10">
+      <div className="flex flex-col gap-y-6">
+        <div className="px-2 md:w-[750px] xl:w-[1160px] md:mx-auto">
+          <h1 className="text-3xl border-l-2 border-orange-600 pl-2">
+            About Development
+          </h1>
+          <div className="flex flex-col gap-y-4 my-4 pl-2">
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Development:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Muhammadismoil
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                {" "}
+                Mirzorahim
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2">
+              Course:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                3
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Group:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Automation
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                of
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                information
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                processing
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600 ">
+                systems.
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="px-2 md:w-[750px] xl:w-[1160px] md:mx-auto">
+          <h1 className="text-3xl border-l-2 border-orange-600 pl-2">
+            About Supervisor
+          </h1>
+          <div className="flex flex-col gap-y-4 my-4 pl-2">
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Supervisor:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Qosimov
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                {" "}
+                Abdunabi
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2">
+              Title of the disease:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Doctor
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                PHD
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Profession:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Automation
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                of
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                information
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                processing
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600 ">
+                systems.
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="px-2 md:w-[750px] xl:w-[1160px] md:mx-auto">
+          <h1 className="text-3xl border-l-2 border-orange-600 pl-2">
+            About this App
+          </h1>
+          <div className="flex flex-col gap-y-4 my-4 pl-2">
+            <span className="border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Language:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                JavaScript,
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                TypeScript
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Framework:
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                React
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                +
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                TypeScript
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Styles:{" "}
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Tailwind
+              </span>
+              <span className="px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                CSS
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1">
+              Description:{" "}
+              <span className=" border-l-2 border-orange-600 px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                This app is a dynamic, user-friendly web application designed to
+                showcase various road signs with descriptions and images. Users
+                can explore different types of road signs, learning about their
+                meanings and uses.
+              </span>
+              <span className=" border-l-2 border-orange-600 px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                The app uses React for the frontend logic and TypeScript for
+                type safety, ensuring robust and maintainable code. Styling is
+                handled with Tailwind CSS, providing a clean and modern UI with
+                responsive design
+              </span>
+            </span>
+            <span className=" border-l-2 border-orange-600 pl-2 flex flex-wrap gap-y-1 ">
+              Attetion:{" "}
+              <span className=" px-1 ml-1 rounded-[2px] bg-orange-100 text-orange-600">
+                Not all symbols have been added to the database yet!.
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
