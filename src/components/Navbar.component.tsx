7d39f25bd50a1de6b25806e9f92b8263ceb8307f
Muhammadismoil <karimovmuhammadismoil22@gmail.com>
2024-11-28 12:57:43 +0500

    updated and chage the styles

--- src/components/Navbar.component.tsx
+++ src/components/Navbar.component.tsx
@@ -9,16 +9,16 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-white border-blue-200 dark:bg-blue-900 mx-auto">
-      <div className=" flex flex-wrap items-center justify-between mx-auto p-4 bg">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-          Rohbalad
+    <div className="shadow-lg">
+      <div className=" flex flex-wrap items-center justify-between mx-auto p-4 bg container">
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-orange-500">
+          <NavLink to="/">Road Signs</NavLink>
         </span>
         <button
           onClick={toggleMenu}
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-blue-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-blue-400 dark:hover:bg-blue-700 dark:focus:ring-blue-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-orange-500 rounded-lg md:hidden hover:bg-orange-100 focus:outline-none focus:ring-2 focus:ring-orange-200 dark:text-orange-400"
           aria-controls="navbar-default"
           aria-expanded={isOpen}
         >
@@ -43,11 +43,15 @@ const Navbar = () => {
           className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-blue-100 rounded-lg bg-blue-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-blue-800 md:dark:bg-blue-900 dark:border-blue-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-orange-100 rounded-lg bg-orange-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
             <li>
               <NavLink
                 to={"/"}
-                className="block py-2 px-3 text-blue-900 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({ isActive }) =>
+                  isActive
+                    ? " block py-2 px-3 text-slate-700 rounded bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                    : "block py-2 px-3 text-slate-700 rounded hover:bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                }
                 aria-current="page"
               >
                 Асосӣ
@@ -55,21 +59,28 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink
-                to={"/"}
-                className="block py-2 px-3 text-blue-900 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to={"/sings"}
+                className={({ isActive }) =>
+                  isActive
+                    ? " block py-2 px-3 text-slate-700 rounded bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                    : "block py-2 px-3 text-slate-700 rounded hover:bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                }
               >
-                Машрутҳо
+                Sings
               </NavLink>
             </li>
             <li>
               <NavLink
-                to={"/"}
-                className="block py-2 px-3 text-blue-900 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white md:dark:hover:bg-transparent "
+                to={"/about"}
+                className={({ isActive }) =>
+                  isActive
+                    ? " block py-2 px-3 text-slate-700 rounded bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                    : "block py-2 px-3 text-slate-700 rounded hover:bg-orange-200 hover:text-orange-700  md:border-0 md:hover:text-orange-700 md:hover:bg-orange-100 md:px-2 transition-colors"
+                }
               >
-                Донишҷӯ
+                About
               </NavLink>
             </li>
-            
           </ul>
         </div>
       </div>
