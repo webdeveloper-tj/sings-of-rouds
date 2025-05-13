e7f3af5dd000fb73e523ca05a0dfb05f3fc125f4
webdeveloper-tj <muhammadismoil11a@gmail.com>
2025-05-13 16:30:11 +0500

    change:the footer name of the development when change the languarge

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,8 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <div className=" bg-orange-500 h-52 flex justify-center items-center mt-5">
       <div>
-        <h1 className="text-white text-3xl text-center">Muhammadismoil Mirzorahim </h1>
+        <h1 className="text-white text-3xl text-center">
+          {t("about.development.names", { returnObjects: true }).map(
+            (name: string) => (
+              <span key={name}>{name} </span>
+            )
+          )}
+        </h1>
       </div>
     </div>
   );
